c75c54f7187ddb2ef47467dc50f350aa36692470
Mohammad Ishan Ahmad <mdishanahmad@gmail.com>
2026-04-20 15:46:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,17 +5,16 @@ let button = document.querySelector("input");
 button.addEventListener("click", (e) => {
   e.preventDefault();
 
-  document.cookie = `fontsize=${sizeEl.value}`;
-  document.cookie = `fontcolor=${colorEl.value}`;
+  document.cookie = `fontsize=${sizeEl.value}; path=/`;
+  document.cookie = `fontcolor=${colorEl.value}; path=/`;
 });
+
 function getCookie(name) {
   const cookies = document.cookie.split("; ");
-
   for (let cookie of cookies) {
     const [key, value] = cookie.split("=");
     if (key === name) return value;
   }
-
   return "";
 }
 
@@ -25,7 +24,15 @@ function loadPreferences() {
 
   if (savedSize) sizeEl.value = savedSize;
   if (savedColor) colorEl.value = savedColor;
+}
 
+function applyPreferences() {
+  const size = getCookie("fontsize");
+  const color = getCookie("fontcolor");
+
+  if (size) document.body.style.fontSize = size + "px";
+  if (color) document.body.style.color = color;
 }
 
 loadPreferences();
+applyPreferences();
